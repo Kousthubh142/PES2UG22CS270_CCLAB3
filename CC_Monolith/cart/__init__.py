62425b54392bd3cc0fb5fd3c2bed454b325e10bf
Kousthubh N Gowda <kousthubh142@gmail.com>
2025-01-21 19:09:27 +0530

    The One for Correction

--- CC_Monolith/cart/__init__.py
+++ CC_Monolith/cart/__init__.py
@@ -1,41 +1,40 @@
 import json
-from typing import List
+
 import products
 from cart import dao
 from products import Product
 
 
 class Cart:
-    def __init__(self, id: int, username: str, contents: List[Product], cost: float):
+    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
         self.id = id
         self.username = username
         self.contents = contents
         self.cost = cost
 
-    @staticmethod
-    def load(data: dict) -> "Cart":
-        return Cart(
-            id=data["id"],
-            username=data["username"],
-            contents=[Product(**item) for item in json.loads(data["contents"])],
-            cost=data["cost"],
-        )
+    def load(data):
+        return Cart(data['id'], data['username'], data['contents'], data['cost'])
 
 
-def get_cart(username: str) -> List[Product]:
+def get_cart(username: str) -> list:
     cart_details = dao.get_cart(username)
-    if not cart_details:
+    if cart_details is None:
         return []
 
-    all_products = []
+    items = []
     for cart_detail in cart_details:
-        try:
-            contents = json.loads(cart_detail["contents"])
-        except json.JSONDecodeError:
-            continue  # Skip invalid JSON contents
-        all_products.extend(products.get_product(item) for item in contents)
-    
-    return all_products
+        contents = cart_detail['contents']
+        evaluated_contents = eval(contents)
+        for content in evaluated_contents:
+            items.append(content)
+
+    i2 = []
+    for i in items:
+        temp_product = products.get_product(i)
+        i2.append(temp_product)
+    return i2
+
+
 
 
 def add_to_cart(username: str, product_id: int):
@@ -44,7 +43,6 @@ def add_to_cart(username: str, product_id: int):
 
 def remove_from_cart(username: str, product_id: int):
     dao.remove_from_cart(username, product_id)
-
 
 def delete_cart(username: str):
     dao.delete_cart(username)
